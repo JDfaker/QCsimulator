331041a4b546a514199b016e2589d3bff5274914
LariDG <larisa.d-g@hotmail.co.uk>
2021-03-17 12:07:47 +0000

    fixed bug

---
+++ shor/quantum_gate.py
@@ -0,0 +1,10 @@
+"""
+This script contains quantum gates in for shors
+"""
+
+import numpy as np
+
+H_Shors = np.array([[1 / np.sqrt(2), 1 / np.sqrt(2)],
+                    [1 / np.sqrt(2), -1 / np.sqrt(2)]])
+
+
